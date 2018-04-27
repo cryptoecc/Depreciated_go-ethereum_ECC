77b42e579a427fbe48bb247b558e74f6acaa42b2
4000D <dkdkajej@gmail.com>
2018-04-27 15:51:54 +0900

    plasma: downloader template

---
+++ plasma/sync.go
@@ -0,0 +1,13 @@
+package plasma
+
+import (
+// "github.com/ethereum/go-ethereum/common"
+)
+
+// Downloader downloads blocks from peers
+type Downloader struct {
+}
+
+// Fetcher contains the block announcement based synchronisation.
+type Fetcher struct {
+}
