e8e67bd24da87060b5a637d46dbc67a6bc1b27a7
Geonwoo Shin <dn2757@gmail.com>
2019-05-17 16:08:47 +0900

    increase block time

--- run.rootchain.sh
+++ run.rootchain.sh
@@ -14,7 +14,7 @@ KEY3="ae03e057a5b117295db86079ba4c8505df6074cdc54eec62f2050e677e5d4e66";
 
 make geth && build/bin/geth \
   --dev \
-  --dev.period 1 \
+  --dev.period 14 \
   --dev.faucetkey "$OPERATOR_PRIV_KEY,$KEY0,$KEY1,$KEY2,$KEY3,$CHALLENGER_KEY" \
   --miner.gastarget 7500000 \
   --miner.gasprice "10" \
